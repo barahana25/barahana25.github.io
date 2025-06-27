bdcf9b64cdc21bfe3ea457a8cb1bc66face06af8
barahana25 <barahana10@kumoh.ac.kr>
2025-06-27 20:42:20 +0900

    Update places.js

--- docs/places.js
+++ docs/places.js
@@ -6,8 +6,8 @@ const loadPlaces = function(coords) {
         {
             name: "Your place name",
             location: {
-                lat: 36.35527, // add here latitude if using static data
-                lng: 128.70246, // add here longitude if using static data
+                lat: 36.35524157455368, // add here latitude if using static data
+                lng: 128.70253036076534, // add here longitude if using static data
 
             }
         },
